c7a37ada78d9b52c3fda53ef51919747f15161ab
Andrew Domingues <drewdomi@proton.me>
2023-04-12 21:21:55 -0300

    fix: use doble quote insted single quote

--- src/components/Button/stories.jsx
+++ src/components/Button/stories.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { theme } from "../../theme/theme";
 import * as S from "./styled";
 
 export default {
@@ -8,15 +7,27 @@ export default {
   args: {
     children: "Texto do Botão",
     color: "dodgerblue",
-    size: "1.1rem"
+    size: "1.1rem",
   },
 };
 
-const s = ({ children, color, size }) => <S.Wrapper size={size} color={color} variant="solid">{children}</S.Wrapper>;
+const s = ({ children, color, size }) => (
+  <S.Wrapper size={size} color={color} variant="solid">
+    {children}
+  </S.Wrapper>
+);
 
-const g = ({ children, color, size }) => <S.Wrapper size={size} color={color} variant="ghost">{children}</S.Wrapper>;
+const g = ({ children, color, size }) => (
+  <S.Wrapper size={size} color={color} variant="ghost">
+    {children}
+  </S.Wrapper>
+);
 
-const t = ({ children, color, size }) => <S.Wrapper size={size} color={color} variant="text">{children}</S.Wrapper>;
+const t = ({ children, color, size }) => (
+  <S.Wrapper size={size} color={color} variant="text">
+    {children}
+  </S.Wrapper>
+);
 
 export const Solid = s.bind({});
 export const Ghost = g.bind({});
--- src/components/Button/styled.jsx
+++ src/components/Button/styled.jsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 import { motion } from "framer-motion";
 
 export const Wrapper = styled(motion.button)`
-  ${({ theme, size, color }) => css`
+  ${({ theme, size }) => css`
     cursor: pointer;
     border: solid 2px;
     padding: 6px 12px;
@@ -13,23 +13,25 @@ export const Wrapper = styled(motion.button)`
     border-radius: 4px;
     font-weight: 700;
     ${({ color, variant }) =>
-      variant === "solid" && `
+      variant === "solid" &&
+      `
       background-color: ${color ? color : theme.colors.primary};
       border-color: ${color ? color : theme.colors.primary};
     `}
-    ${({color, variant}) =>
-      variant === "ghost" && `
+    ${({ color, variant }) =>
+      variant === "ghost" &&
+      `
       background-color: transparent;
-      color: ${color ? color: theme.colors.primary};
+      color: ${color ? color : theme.colors.primary};
       border-color: ${color ? color : theme.colors.primary};
     `}
-    ${({color, variant}) =>
-      variant === "text" && `
+    ${({ color, variant }) =>
+      variant === "text" &&
+      `
       background-color: transparent;
       border: none;
-      color: ${color ? color: theme.colors.primary};
+      color: ${color ? color : theme.colors.primary};
       text-decoration: underline;
     `}
-
   `},
 `;
